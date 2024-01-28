6118543dc5051dcb65f19a2936af78cef506ba1e
Flux <flustix@foxes4life.net>
2024-01-28 08:46:12 +0100

    add json stuff to `Comment`

--- CamelliaWiki.Backend/Components/Comments/Comment.cs
+++ CamelliaWiki.Backend/Components/Comments/Comment.cs
@@ -1,40 +1,47 @@
 ﻿using MongoDB.Bson;
 using MongoDB.Bson.Serialization.Attributes;
+using Newtonsoft.Json;
 
 namespace CamelliaWiki.Backend.Components.Comments;
 
 public class Comment
 {
     [BsonId]
+    [JsonProperty("id")]
     public ObjectId ID { get; set; } = ObjectId.GenerateNewId();
 
     /// <summary>
     /// Discord ID of the author of this comment.
     /// </summary>
     [BsonElement("author")]
+    [JsonProperty("author")]
     public ulong AuthorID { get; set; } = 0;
 
     /// <summary>
     /// The content of this comment.
     /// </summary>
     [BsonElement("content")]
+    [JsonProperty("content")]
     public string Content { get; set; } = "";
 
     /// <summary>
     /// The slug of the post this comment is on.
     /// </summary>
     [BsonElement("slug")]
+    [JsonProperty("slug")]
     public string PostSlug { get; set; } = "";
 
     /// <summary>
     /// The timestamp of this comment.
     /// </summary>
     [BsonElement("time")]
+    [JsonProperty("time")]
     public long Timestamp { get; set; } = DateTimeOffset.UtcNow.ToUnixTimeSeconds();
 
     /// <summary>
     /// The ID of the parent comment. Used for replies. Null if this is a top-level comment.
     /// </summary>
     [BsonElement("parent")]
+    [JsonProperty("parent")]
     public ObjectId? ParentID { get; set; }
 }
