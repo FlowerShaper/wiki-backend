61354f5716e69867af0f191361834c2a82242be5
flustix <me@flux.moe>
2025-10-27 22:46:42 +0100

    cdn route

---
+++ CamelliaWiki.Backend/API/Routes/AssetRoute.cs
@@ -0,0 +1,30 @@
+﻿using CamelliaWiki.Backend.API.Components;
+using Midori.Networking;
+
+namespace CamelliaWiki.Backend.API.Routes;
+
+public class AssetRoute : IWikiAPIRoute
+{
+    public string RoutePath => "/cdn";
+    public HttpMethod Method => HttpMethod.Get;
+
+    public async Task Handle(WikiAPIInteraction interaction)
+    {
+        if (!interaction.TryGetStringQuery("path", out var path))
+        {
+            await interaction.ReplyError(HttpStatusCode.BadRequest, "Missing 'path' query.");
+            return;
+        }
+
+        var fs = Path.Combine(Program.Config.DataDirectory, "_assets", path);
+
+        if (!File.Exists(fs))
+        {
+            await interaction.ReplyError(HttpStatusCode.NotFound, "");
+            return;
+        }
+
+        var data = await File.ReadAllBytesAsync(fs);
+        await interaction.ReplyData(data, Path.GetFileName(fs));
+    }
+}
